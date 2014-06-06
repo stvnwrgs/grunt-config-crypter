1383f2e6acb88163b3f870fc9cb3c8582f1f61a9
Steven Wirges <stevenwirges@Stevens-MacBook-Pro.local>
2014-06-06 15:36:17 +0200

    added not encrpted check

--- tasks/config_crypter.js
+++ tasks/config_crypter.js
@@ -17,36 +17,36 @@ module.exports = function(grunt) {
   };
 
   var decrypt = function (string, password) {
-    var decrypted = CryptoJS.AES.decrypt(string, password);
+    //Check if string is not encrypted
+    if (string.substring(0,9) != "encrypted")
+      return string;
+   
+    var decrypted = CryptoJS.AES.decrypt(plainify(string), password);
     var hexParsed = CryptoJS.enc.Hex.parse(decrypted.toString());
     return decryptify(hexParsed.toString(CryptoJS.enc.Utf8));
   };
 
   var encryptify = function (string) {
     return "encrypted("+string+")";
-
   };
 
   var decryptify = function (string) {
     return "decrypted("+string+")";
   };
 
-  var playnify = function (string) {
+  //removed encrypted and decrypted
+  var plainify = function (string) {
     return string.substring(0,string.length-1).replace("encrypted(", "").replace("decrypted(", "");
   };
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('config_crypter', 'A Grunt Plugin for encrypting credentials in config files. Develop your app, create your configs. Then add your credentials and encrypt them. Push you changes, let them decrypt by your ci before you push the code to your server', function(func,password) {
-    // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
     });
       
     // Iterate over all specified file groups.
     this.data.configs.forEach(function(f) {
         // Warn on and remove invalid source files (if nonull was set).
-      console.log("UIASHDKASH");
 
       if (!grunt.file.exists(f)) {
         grunt.log.warn('Source file "' + f + '" not found.');
@@ -60,7 +60,7 @@ module.exports = function(grunt) {
       switch(func) {
           case "plain":
             task = function (string,passwrd) {
-              return playnify(decrypt(string,password));
+              return plainify(decrypt(string,password));
             };
             break;
           case "encrypt":
@@ -76,17 +76,13 @@ module.exports = function(grunt) {
 
 
       variables.forEach(function(variable) {
-        // console.log(func(playnify(variable),password));
-        var newValue = task(playnify(variable),password);
-        console.log(newValue);
+        var newValue = task(variable,password);
+        grunt.log.writeln("Var: " + variable + " changed to: " + newValue );
         fileContent = fileContent.replace(variable,newValue);
       });  
 
-      console.log(fileContent);
       grunt.file.write(f, fileContent);
 
-
-      // Print a success message.
       grunt.log.writeln('File "' + f + '" '+ func+'ed.');
     });
   });
